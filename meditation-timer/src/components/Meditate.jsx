b0bb22172b4050c5d304d3d82909770823d3c852
ri-ca4 <riley.de12@gmail.com>
2023-05-03 17:58:23 -0400

    set and display timer

--- meditation-timer/src/components/Meditate.jsx
+++ meditation-timer/src/components/Meditate.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Meditate = () => {
+  const [userInput, setUserInput] = useState();
+  let mins = 10;
+  let secs = 0;
+
+  const handleInput = (e)=>{
+    setUserInput(e.target.value);
+  }
+
+  function displayTime(){
+    let dispMins  = document.getElementById('minutes');
+    let dispSecs  = document.getElementById('seconds');
+
+    dispMins.innerHTML = mins;
+    if(secs < 10){
+        dispSecs.innerHTML= '0' + secs;
+    }else{
+        dispSecs.innerHTML = secs;
+    }
+  }
+
+  const handleSet = ()=>{
+    if(userInput != null){
+      mins = userInput;
+    }
+    secs = 0;
+    displayTime();
+  }
+  
+
+
   return (
-    <div>
-      Meditate
+    <div className='meditate'>
+      <label>Minutes:</label>
+        <input type="number" id="userInput" value={userInput} onChange={handleInput}/>
+        <button id="set" onClick={handleSet}>Set Timer</button>
+        <div id="meditate-timer">
+            <h1>
+                <span id="minutes"></span>:
+                <span id="seconds"></span>
+            </h1>
+            <button id="start">Start</button>
+            <button id="pause">Pause</button>
+        </div>
     </div>
   )
 }
