194f054544701b59e9c838f55eb5cb7272513ed2
ri-ca4 <riley.de12@gmail.com>
2023-05-03 20:15:22 -0400

    add timer functionality, fix bugs, toggle disabled buttons

--- meditation-timer/src/components/Meditate.jsx
+++ meditation-timer/src/components/Meditate.jsx
@@ -1,33 +1,73 @@
 import React from 'react'
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { useState, useRef } from 'react';
 
 const Meditate = () => {
   const [userInput, setUserInput] = useState();
-  let mins = 10;
-  let secs = 0;
+  const [isRunning, setIsRunning] = useState(false);
+  let mins = useRef(10);
+  let secs = useRef(0);
 
   const handleInput = (e)=>{
     setUserInput(e.target.value);
   }
 
-  function displayTime(){
+  const displayTime = ()=>{
     let dispMins  = document.getElementById('minutes');
     let dispSecs  = document.getElementById('seconds');
 
-    dispMins.innerHTML = mins;
-    if(secs < 10){
-        dispSecs.innerHTML= '0' + secs;
+    dispMins.innerHTML = mins.current;
+    if(secs.current < 10){
+        dispSecs.innerHTML= '0' + secs.current;
     }else{
-        dispSecs.innerHTML = secs;
+        dispSecs.innerHTML = secs.current;
     }
   }
 
-  const handleSet = ()=>{
-    if(userInput != null){
-      mins = userInput;
+  function handleSet() {
+    document.getElementById('start').removeAttribute("disabled");
+    document.getElementById('pause').setAttribute("disabled", "true");
+    if (userInput != null) {
+      mins.current = userInput;
+    }else{
+      mins.current = 10;
     }
-    secs = 0;
+    secs.current = 0;
     displayTime();
+
+  }
+
+
+  useEffect(() => {
+    if(isRunning){
+      const interval = setInterval(()=>{
+        if (mins.current === 0 && secs.current <=0) {
+          clearInterval();
+          secs.current= 0;
+          setIsRunning(false);
+        }else{
+          if(secs.current === 0){
+              mins.current = mins.current-1;
+              secs.current =60;
+          }
+          secs.current = secs.current-1 ;
+          displayTime();
+          }
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [isRunning, displayTime]);
+
+  const handleStart = ()=>{
+    document.getElementById('start').setAttribute("disabled", "true");
+    document.getElementById('pause').removeAttribute("disabled");
+    setIsRunning(true);
+  }
+
+  const handlePause = ()=>{
+    document.getElementById('start').removeAttribute("disabled");
+    document.getElementById('pause').setAttribute("disabled", "true");
+    setIsRunning(false);
   }
   
 
@@ -35,15 +75,15 @@ const Meditate = () => {
   return (
     <div className='meditate'>
       <label>Minutes:</label>
-        <input type="number" id="userInput" value={userInput} onChange={handleInput}/>
+        <input type="number" id="userInput" onChange={handleInput}/>
         <button id="set" onClick={handleSet}>Set Timer</button>
         <div id="meditate-timer">
             <h1>
                 <span id="minutes"></span>:
                 <span id="seconds"></span>
             </h1>
-            <button id="start">Start</button>
-            <button id="pause">Pause</button>
+            <button id="start" onClick={handleStart}>Start</button>
+            <button id="pause" onClick={handlePause}>Pause</button>
         </div>
     </div>
   )
